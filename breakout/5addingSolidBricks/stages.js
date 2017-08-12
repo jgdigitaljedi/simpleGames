936c698cc6b9bc0ef7143804b13e235ab50cb2b3
Joey Gauthier <digitaljedi@outlook.com>
2017-08-12 11:39:53 -0500

    got other stages refactored to work with new bricks and blocks setup

--- breakout/5addingSolidBricks/stages.js
+++ breakout/5addingSolidBricks/stages.js
@@ -34,28 +34,39 @@
             return stage;
         },
         stage2: function () {
-            var stage = [];
+            var stage = {
+                bricks: [],
+                blocks: [],
+                brickCount: 0
+            };
             window.defaults.brickRowCount = 8;
             for (var c = 0; c < defaults.brickColumnCount; c++) {
-                stage[c] = [];
+                stage.bricks[c] = [];
+                stage.blocks[c] = [];
                 for (var r = 0; r < defaults.brickRowCount; r++) {
                     var brickX = (c*(defaults.brickWidth + defaults.brickPadding)) + defaults.brickOffsetLeft;
                     var brickY = (r*(defaults.brickHeight + defaults.brickPadding)) + defaults.brickOffsetTop;
-                    stage[c][r] = {x: brickX, y: brickY};
+                    stage.bricks[c][r] = {x: brickX, y: brickY};
                     if (c < r) {
-                        stage[c][r].show = false;
+                        stage.bricks[c][r].show = false;
                     } else {
-                        stage[c][r].show = true;
+                        stage.bricks[c][r].show = true;
+                        stage.brickCount++;
                     }
                 }
             }
             return stage;
         },
         stage3: function () {
-            var stage = [];
+            var stage = {
+                bricks: [],
+                blocks: [],
+                brickCount: 0
+            };
             window.defaults.brickRowCount = 8;
             for (var c = 0; c < defaults.brickColumnCount; c++) {
-                stage[c] = [];
+                stage.bricks[c] = [];
+                stage.blocks[c] = [];
                 for (var r = 0; r < defaults.brickRowCount; r++) {
                     var brickX = (c*(defaults.brickWidth + defaults.brickPadding)) + defaults.brickOffsetLeft;
                     var brickY = (r*(defaults.brickHeight + defaults.brickPadding)) + defaults.brickOffsetTop;
@@ -65,7 +76,8 @@
                     } else {
                         showBrick = c % 2 === 1;
                     }
-                    stage[c][r] = {x: brickX, y: brickY, show: showBrick};
+                    if (showBrick) stage.brickCount++;
+                    stage.bricks[c][r] = {x: brickX, y: brickY, show: showBrick};
                 }
             }
             return stage;
